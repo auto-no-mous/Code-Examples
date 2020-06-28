d8d7ad6b601d581b1ceca65eef2d5b66305329f6
auto-no-mous <67557657+auto-no-mous@users.noreply.github.com>
2020-06-28 22:38:21 +0300

    Add files via upload

---
+++ 4. Создание и обработка двумерного массива.cs
@@ -0,0 +1,67 @@
+﻿//Задача. Сгенерировать двухмерный массив 5×5 случайных чисел(в диапазоне  [-10;10]). 
+//Вывести массив на экран.
+//Найти сумму модулей отрицательных нечетных элементов.
+
+using System;
+
+
+namespace MultidimensionalArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] m = new int[5, 5];
+            FillMatrix(m);
+            ShowMatrix(m);
+            Console.WriteLine("Модуль суммы отрицательных нечетных чисел = {0}", SearchInMatrix(m));
+        }
+
+        static void ShowMatrix(int[,] a)
+        {
+            int rows = a.GetUpperBound(0) + 1;
+            int columns = a.GetUpperBound(1) + 1;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write("{0,4} ", a[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void FillMatrix(int[,] a)
+        {
+            int rows = a.GetUpperBound(0) + 1;
+            int columns = a.GetUpperBound(1) + 1;
+            Random rand = new Random();
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    a[i, j] = rand.Next(-10, 10);
+                }
+            }
+        }
+
+        static int SearchInMatrix(int[,] a)
+        {
+            int rows = a.GetUpperBound(0) + 1;
+            int columns = a.GetUpperBound(1) + 1;
+            int answer = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if ((a[i, j] % 2 == 0) && (a[i, j] < 0))
+                    {
+                        answer += a[i, j];
+                    }
+                }
+            }
+            return Math.Abs(answer);
+        }
+    }
+
+}
---
+++ 5. Перечисления - пример использования.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+
+namespace FunWithEnums
+{
+    // объявление перечисления
+    enum EmpType
+    {
+        Manager,       // = 0
+        Grunt,         // = 1
+        Contractor,    // = 2
+        VicePresident  // = 3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Fun with Enums *****");
+            //создаётся экземпляр перечисления
+            EmpType emp = EmpType.Contractor;
+            AskForBonus(emp);
+
+            // выводит "emp is a Contractor".
+            Console.WriteLine("emp is a {0}.", emp.ToString());
+
+            // выводит "Contractor = 100".
+            Console.WriteLine("{0} = {1}", emp.ToString(), (byte)emp);
+
+            EmpType e2 = EmpType.Contractor;
+
+            // Эти перечисления объявлены в пространстве имен System
+            DayOfWeek day = DayOfWeek.Monday;
+            ConsoleColor cc = ConsoleColor.Gray;
+            EvaluateEnum(e2);
+            EvaluateEnum(day);
+            EvaluateEnum(cc);
+
+            Console.ReadLine();
+        }
+
+        #region Enum as parameter
+        // передача перечислений в качестве аргументов
+        static void AskForBonus(EmpType e)
+        {
+            switch (e)
+            {
+                case EmpType.Manager:
+                    Console.WriteLine("How about stock options instead?");
+                                     //He желаете ли взамен фондовые опционы?
+                    break;
+                case EmpType.Grunt:
+                    Console.WriteLine("You have got to be kidding...");
+                                    // Вы должно быть шутите. . .
+                    break;
+                case EmpType.Contractor:
+                    Console.WriteLine("You already get enough cash...");
+                                    // Вы уже получаете вполне достаточно...
+                    break;
+                case EmpType.VicePresident:
+                    Console.WriteLine("VERY GOOD, Sir!");
+                                    // Очень хорошо, сэр!
+                    break;
+            }
+        }
+        #endregion
+
+        #region Analyse Enum
+        // этот метод выводит подробности о переданном в аргументе перечислении
+        static void EvaluateEnum(System.Enum e)
+        {
+            Console.WriteLine("=> Information about {0}", e.GetType().Name);
+
+            Console.WriteLine("Underlying storage type: {0}",
+              Enum.GetUnderlyingType(e.GetType()));
+            // Получить все пары "имя-значение" для входного параметра.
+            Array enumData = Enum.GetValues(e.GetType());
+            Console.WriteLine("This enum has {0} members.", enumData.Length);
+            // Вывести строковое имя и ассоциированное значение,
+            // используя флаг формата D
+            for (int i = 0; i < enumData.Length; i++)
+            {
+                Console.WriteLine("Name: {0}, Value: {0:D}",
+                  enumData.GetValue(i));
+            }
+            Console.WriteLine();
+        }
+        #endregion
+    }
+}
