b9bc60d44d13a69144b2adb56d3414b6cbc726cc
auto-no-mous <67557657+auto-no-mous@users.noreply.github.com>
2020-09-05 17:27:30 +0300

    Add files via upload

---
+++ 013. Классы - задача о комплексных числах.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Transactions;
+
+namespace ConsoleApplication1
+{
+    class Complex
+    {
+        public double real;
+        public double imaginary;
+
+        public Complex (double r=0, double f=0)
+        {
+            real = r;
+            imaginary = f;
+        }
+
+        public bool TryReadNumber()
+        {
+            try 
+            {
+                double r;
+                Console.Write("Real: ");
+                r = double.Parse(Console.ReadLine());
+
+                double i;
+                Console.Write("Imaginary: ");
+                i = double.Parse(Console.ReadLine());
+
+                real = r;
+                imaginary = i;
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public string ToString ()
+        {
+            return "(" + real.ToString() + ", " + imaginary.ToString() + "i)";
+        }
+
+        public Complex Conjugate ()
+        {
+            return new Complex(real, imaginary * -1);
+        }
+
+        public Complex Add(Complex b)
+        {
+            return new Complex(real + b.real, imaginary+b.imaginary);
+        }
+
+        public Complex Substract(Complex b)
+        {
+            return new Complex(real - b.real, imaginary-b.imaginary);
+        }
+
+        public Complex Multiply(Complex b)
+        {
+            return new Complex(real * b.real-imaginary*b.imaginary, real*b.imaginary + imaginary*b.real);
+        }
+        public Complex Multiply(double x)
+        {
+            return new Complex(real * x, imaginary * x);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex a = new Complex(3.0, 4.0); // инициализация значением (3,4і) 
+            Complex c = new Complex();
+            Console.WriteLine("Enter a complex number (q to quit) :");
+            // Ввод комплексного числа (q для завершения) 
+            while (c.TryReadNumber()) 
+            {
+                // значение с 
+                Console.WriteLine("\nс is " + c.ToString());
+                // значение сопряженного числа 
+                Console.WriteLine("complex conjugate is " + c.Conjugate().ToString());
+                // значение а
+                Console.WriteLine("a is " + a.ToString());
+                // значение а + с 
+                Console.WriteLine("a + с is " + a.Add(c).ToString());
+                // значение а — с 
+                Console.WriteLine("a - с is " + a.Substract(c).ToString());
+                // значение а * с 
+                Console.WriteLine("a * с is " + a.Multiply(c).ToString());
+                // значение 2 * с 
+                Console.WriteLine("с * 2 is " + c.Multiply(2).ToString());
+                Console.Write("Enter a complex number (q to quit) :\n");
+            }
+            Console.WriteLine("Done!");
+        }
+
+    }
+}
---
+++ 014. Декструктор и сборщик мусора.cs
@@ -0,0 +1,77 @@
+﻿
+using System;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    public class Person
+    {
+        private static int population = 0;
+        public string name;
+        public int age;
+
+        public Person(string n = "Неизвестно", int a = 18)
+        { name = n; age = a; population++; }//конструктор
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Имя: {name}  Возраст: {age}");
+        }
+
+        public static int Population
+        {
+            get
+            {
+                return population;
+            }
+        }
+
+        ~Person()
+        {
+            population--;
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            {
+                Person p1 = new Person("Vasya", 22);
+                Person p2 = new Person("Petya");
+
+                Console.WriteLine("Всего объектов Person: {0}", Person.Population);
+                p1.GetInfo();
+                p2.GetInfo();
+            }
+
+            Console.Write("G - force call GC, N - create new Person, or Q to quit (press enter to refresh): ");
+            string input = Console.ReadLine().ToLower();
+            while (input != "q")
+            {
+                if (input == "g")
+                {
+
+                    GC.Collect();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Всего объектов Person: {0}", Person.Population);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                else if (input == "n")
+                {
+                    Person p4 = new Person();
+                    p4.GetInfo();
+                }
+                else
+                {
+                    Console.WriteLine("Всего объектов Person: {0}", Person.Population);
+                }
+                Console.Write("G - call garbage collector, Q - quit, or press enter to refresh: ");
+                input = Console.ReadLine().ToLower();
+            }
+        }
+    }
+}
+
+
